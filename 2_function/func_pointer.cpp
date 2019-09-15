48e74e13aac22dd495c23bf2a87b7a8787879c78
hezl1592 <hezl1592@163.com>
2019-09-15 14:59:20 +0800

    func pointer

---
+++ 2_function/func_pointer.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+double betsy(int);
+double pam(int);
+
+void estimate(int lines, double (*pf)(int));
+int main()
+{
+    int code;
+    cout << "How many lines of code do you need? ";
+    cin >> code;
+    cout << "Here is Betsy's estimate:\n";
+    estimate(code, betsy);
+    cout << "Here is Pam's estimate: \n";
+    estimate(code, pam);
+
+    return 0;
+}
+
+double betsy(int lns)
+{
+    return 0.05*lns;
+}
+
+double pam(int lns)
+{
+    return 0.03 * lns * 0.0004 * lns*lns;
+}
+
+void estimate(int lines, double(*pf)(int))
+{
+    cout << lines<<" lines will take ";
+    cout << (*pf)(lines) << " hour(s)\n";
+}
