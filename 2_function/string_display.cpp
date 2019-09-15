1c5f416e4d4f6c825d291f077562f8278f126984
hezl1592 <hezl1592@163.com>
2019-09-15 09:34:38 +0800

    use string

---
+++ 2_function/string_display.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int SIZE = 5;
+void display(const string sa[], int n);
+
+
+int main()
+{
+    string list[SIZE];
+    cout << "Enter your "<<SIZE<< "favorite astronomicak sights:\n";
+
+    for (int i=0; i<SIZE; i++)
+    {
+        cout << i+1<<": ";
+        getline(cin, list[i]);
+    }
+
+    cout << "Yout list: \n";
+    display(list, SIZE);
+    return 0;
+}
+
+void display(const string sa[], int n)
+{
+    for (int i=0; i<n; i++)
+        cout <<i+1<<": "<<sa[i]<<endl;
+}
